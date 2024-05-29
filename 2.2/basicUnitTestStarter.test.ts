bef79dd2875e6547c6ec3c1bdd45e31b0959de5a
unknown <wbl.work.email@gmail.com>
2024-05-29 12:21:44 -0500

    2.3 Homework

--- 2.2/basicUnitTestStarter.test.ts
+++ 2.2/basicUnitTestStarter.test.ts
@@ -2,19 +2,19 @@ describe("Testing the calculator", () => {
     test("Example test", () => {
       expect(calculator.add(2, 50)).toBe(52);
     });
-    test("Addition WBL15", () => {
+    test("Addition works", () => {
       expect (calculator.add(5,6)).toBe(11);
     });
-    test("Subtraction WBL15" () => {
+    test("Subtraction works" () => {
       expect(calculator.subtract(6,5)).toBe(1);
     });
-    test("Multiplication WBL15", () => {
+    test("Multiplication works", () => {
       expect(calculator.multiply(5,6)).toBe(30);
     });
-    test("Division WBL15", () => {
+    test("Division works", () => {
       expect(calculator.divide(6,3)).toBe(2);
     });
-    describe("Stretch Goals 1 WBL15", () => {
+    describe("Stretch Goals 1", () => {
       test("(9+3)/4 == 3", () => {
         expect(calculator.divide(calculator.add(9,3), 4)).toBe(3);
       });
